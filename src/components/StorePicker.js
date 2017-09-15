8438fb266f014d76fb976ab010f75baa44c87d57
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-09-15 14:06:36 +0200

    add helpers

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import { getName } from '../helper'
 export default class StorePicker extends React.Component {
   render() {
     return (
-
       <form className='store-selector' >
         <h2>Enter store</h2>
         { /* This is a comment in JSX */ }
-        <input type='text' required />
+        <input type='text' required
+          defaultValue={getName} />
         <hr/>
         <br/>
       </form>
